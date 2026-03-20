9a22ed76dd7da00073c86e3c7e9373ddd2f8230b
Mohit Chauhan <mohitchauhan45220@gmail.com>
2026-03-20 14:32:35 +0530

    add vite config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      // 1. Force Vite to check for changes (fixes WSL/Windows issues)
+      usePolling: true, 
+    },
+    // 2. Ensures the HMR connection remains stable
+    hmr: {
+      overlay: true, 
+    },
+  },
 })

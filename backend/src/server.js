df3c59a382288933de975a59ac817bc1e1cd8431
Mohit Chauhan <mohitchauhan45220@gmail.com>
2026-04-01 10:27:06 +0530

    add local & production urls

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-  origin: "https://room-rent-avalability-system.vercel.app"
+  origin: process.env.CLIENT_URL
 }))
 app.use(express.json())
 

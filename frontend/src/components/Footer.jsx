a10fa920678c000c049b757dad953684c2a12352
Mohit Chauhan <mohitchauhan45220@gmail.com>
2026-04-01 12:12:52 +0530

    add change tailwing config

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
 
 const Footer = () => {
   return (
     <div>
-      <p>StayNest 2026 @ all rights reservered</p>
-
-      <h1 className="text-3xl text-blue-500 font-bold">
-        Tailwind Working 🚀
+      <p className="bg-white">
+        StayNest 2026 @ all rights reservered
+      </p>
+      <h1 className="text-5xl text-red-500 font-bold">
+        NOW IT WILL WORK 🚀
       </h1>
     </div>
-
+  
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    watch: {
-      // 1. Force Vite to check for changes (fixes WSL/Windows issues)
-      usePolling: true, 
-    },
-    // 2. Ensures the HMR connection remains stable
-    hmr: {
-      overlay: true, 
-    },
-  },
+  plugins: [react()],
 })

6402a4dc1f910b9854fd81a7d346d3dcd9e892e0
Mohit Chauhan <mohitchauhan45220@gmail.com>
2026-04-01 11:05:49 +0530

    add tailwind config files

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,12 @@ const Footer = () => {
   return (
     <div>
       <p>StayNest 2026 @ all rights reservered</p>
+
+      <h1 className="text-3xl text-blue-500 font-bold">
+        Tailwind Working 🚀
+      </h1>
     </div>
+
   )
 }
 

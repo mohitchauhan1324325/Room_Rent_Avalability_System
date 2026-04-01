585bb7930c4c29f90944a516afe10d7fd2ac645c
Mohit Chauhan <mohitchauhan45220@gmail.com>
2026-04-01 12:19:09 +0530

    add style

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,11 +5,8 @@ const Footer = () => {
       <p className="bg-white">
         StayNest 2026 @ all rights reservered
       </p>
-      <h1 className="text-5xl text-red-500 font-bold">
-        NOW IT WILL WORK 🚀
-      </h1>
     </div>
-  
+
   )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,6 @@ const Navbar = () => {
         <nav className="bg-gray-900 text-white px-6 py-3 flex items-center gap-6">
             <img src="transparent-logo.png" alt="logo" style={{ width: "70px", height: "80px" }} />
             <h1 className="text-xl font-bold">StayNest</h1>
-            <p>Smart Room Availability & Rent Booking Platform</p>
             <div className="flex gap-6">
                 <Link to="/" className="hover:text-gray-300">Home</Link>
                 <Link to="/rooms" className="hover:text-gray-300">Rooms </Link>

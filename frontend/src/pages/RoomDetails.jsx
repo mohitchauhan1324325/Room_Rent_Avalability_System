c5fb2563d065ab5b1635f69e5ac0426d139e3f3e
Mohit Chauhan <mohitchauhan45220@gmail.com>
2026-04-07 15:12:31 +0530

    add change

--- frontend/src/pages/RoomDetails.jsx
+++ frontend/src/pages/RoomDetails.jsx
@@ -11,28 +11,34 @@ const RoomDetails = () => {
         const fetchDetails = async () => {
             try {
                 const res = await getRoomById(id);
+
+                if (res.message) {
+                    alert(res.message);
+                    return;
+                }
+
                 setRoom(res);
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
+
         fetchDetails();
     }, [id]);
 
+    if (!room) return <p>Loading...</p>;
 
     return (
         <div>
-            <h1>{room?.title}</h1>
-            {room && (
-                <img
-                    src={`${import.meta.env.VITE_API_URL}${room.image}`}
-                    alt="room"
-                />
-            )}
-            <p>Description: {room?.description}</p>
-            <p>Price: {room?.price}</p>
-            <p>Location: {room?.location}</p>
-            <p>Owener: {room?.owner}</p>
+            <h1>{room.title}</h1>
+            <img
+                src={`${import.meta.env.VITE_API_URL}${room.image}`}
+                alt="room"
+            />
+            <p>Description: {room.description}</p>
+            <p>Price: {room.price}</p>
+            <p>Location: {room.location}</p>
+            <p>Owener: {room.owner}</p>
         </div>
     )
 }
